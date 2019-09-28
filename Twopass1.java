f90456a46b190c8ace54eeb1175ebac6687446c2
Deepak Ravtani <54829726+DeepakRav@users.noreply.github.com>
2019-09-28 11:58:28 +0530

    Add files via upload

--- Twopass1.java
+++ Twopass1.java
@@ -1,5 +1,6 @@
 //import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
@@ -7,23 +8,52 @@ class Twopass1
 {
 	public static void main(String args[]) throws IOException
 	{
-		int ch;
+		int ch,count=0,i,flag=0;
 		String str;
+		String name[]={"START", "READ", "MOVER", "MOVEM"};
+		String str1;
 		//File fr=new File("Assemblycode.txt");
 	    BufferedReader input=new BufferedReader(new FileReader("Assemblycode.txt"));
-		System.out.print("Hello");
-		System.out.print((char)10);
 		/*while((str=input.readLine())!=null)
 		{
 			System.out.println(str);
 		}*/
+		FileWriter fw=new FileWriter("mnemonic.txt");
+		FileWriter fw1=new FileWriter("symtab.txt");
 		while((str=input.readLine())!=null)
 		{
 			StringTokenizer st1=new StringTokenizer(str, " ");
+			System.out.println(str);
 			while(st1.hasMoreTokens())
 			{
-				System.out.println(st1.nextToken());
+				count++;
+				str1=st1.nextToken();
+				if(count==1)
+				{
+					for(i=0;i<name.length;i++)
+					{
+						if(str1.equals(name[i]))
+						{
+							fw.write(name[i] + " ");
+							flag=1;
+							System.out.println("Trial!"+ name[i]);
+						}
+					}
+					System.out.println(flag);
+					if(flag!=1)
+					{
+						fw1.write(str1);
+						System.out.println(str1+flag);
+					}
+					
+				}
+					System.out.println(str1);
+					System.out.println(count);
 			}
+			count=0;
+			flag=0;
 		}
+		fw.close();
+		fw1.close();
 	}
 }
