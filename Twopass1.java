2948c1d1f06d29b0b212cf7f0f60d9eec37b2fac
Deepak Ravtani <54829726+DeepakRav@users.noreply.github.com>
2019-10-17 11:55:06 +0530

    Update Twopass1.java

--- Twopass1.java
+++ Twopass1.java
@@ -8,7 +8,7 @@ class Twopass1
 {
 	public static void main(String args[]) throws IOException
 	{
-		int ch,countt=0,i,flag=0,countl=0,lc=0,flageq=0,elc=0;
+		int ch,countt=0,i,flag=0,countl=0,lc=0,flageq=0,elc=0,flaglt=0,countp=0,flagend=0,endlit=0;
 		String str;
 		String name[]={"START", "ADD", "SUB", "MULT", "DIV", "MOVER", "MOVEM", "COMP", "BC", "READ", "PRINT", "STOP", "END"};
 		String str1="",str2="";
@@ -20,7 +20,11 @@ class Twopass1
 		}*/
 		FileWriter fw=new FileWriter("mnemonic.txt");
 		FileWriter fw1=new FileWriter("symtab.txt");
+		FileWriter fw2=new FileWriter("littab.txt");
+		FileWriter fw3=new FileWriter("pooltab.txt");
 		fw1.write("Symbols\tLC\r\n\r\n");
+		fw2.write("Literals\tLC\r\n\r\n");
+		fw3.write("Literal No.\r\n\r\n");
 		while((str=input.readLine())!=null)
 		{
 			countl++;
@@ -34,35 +38,75 @@ class Twopass1
 			{
 				countt++;
 				str1=st1.nextToken();
-				if(countt==1)
+				if(flagend!=1)
 				{
-					for(i=0;i<name.length;i++)
-					{
-						if(str1.equals(name[i]))
+					if(countt==1)
+					{		
+						if(str1.equals("END"))
 						{
-							fw.write(name[i] + "\r\n");
+							flagend=1;
+							lc--;
+						}
+						if(str1.equals("LTORG"))
+						{
+							flaglt=1;
+							lc--;
+							if(countp==0)
+							{
+								countp++;
+							}
+							fw3.write("#" + countp + "\r\n");
+							continue;
+						}
+						for(i=0;i<name.length;i++)
+						{
+							if(str1.equals(name[i]))
+							{
+								fw.write(name[i] + "\r\n");
+								flag=1;
+							}
+						}
+						if(flaglt==1)
+						{
+							if(str1.substring(0,2).equals("='"))
+							{
+								fw2.write(str1 + "\t\t" + lc + "\r\n");
+								countp++;
+							}
+							else
+							{
+								flaglt=0;
+							}
 							flag=1;
 						}
+						if(flag!=1)
+						{
+							str2=str2 + "\t" + str1;
+							fw1.write(str1);
+						}
+						
 					}
-					if(flag!=1)
+					if(countl==1 && countt==2)
 					{
-						str2=str2 + "\t" + str1;
-						fw1.write(str1);
+						lc=Integer.parseInt(str1);
 					}
-					
+					if(str1.equals("EQU"))
+					{
+						flageq=1;
+					}
+						//System.out.println(str1);
+						//System.out.println(countt);
 				}
-				if(countl==1 && countt==2)
+				else
 				{
-					lc=Integer.parseInt(str1);
+					if(str1.substring(0,2).equals("='"))
+					{
+						fw2.write(str1 + "\t\t" + lc + "\r\n");
+						endlit++;
+					}
 				}
-				if(str1.equals("EQU"))
-				{
-					flageq=1;
-				}
-					//System.out.println(str1);
-					//System.out.println(countt);
 			}
-			if(flag!=1)
+			if(flag!=1 && flaglt!=1 && flagend!=1)
 			{
 				if(flageq==1)
 				{
@@ -77,6 +121,7 @@ class Twopass1
 					}
 					str2=str2 + "\t" + elc;
 					fw1.write("\t" + elc + "\r\n");
+					lc--;
 				}
 				else
 				{
@@ -88,7 +133,13 @@ class Twopass1
 			flag=0;
 			flageq=0;
 		}
+		if(endlit>0)
+		{
+			fw3.write("#" + countp + "\r\n");
+		}
 		fw.close();
 		fw1.close();
+		fw2.close();
+		fw3.close();
 	}
 }
